ee948dc6461d17d1944925bcbffafb9fff96f810
Mafruha Levin <mafruhalevin@Fahids-MacBook-Pro-2.local>
2024-11-08 22:46:00 +0200

    first commit

--- service1/service1.py
+++ service1/service1.py
@@ -1,44 +1,55 @@
-import os
+from flask import Flask, jsonify, request
 import socket
-import json
-import requests
-from flask import Flask, jsonify
-import subprocess
+import os
+import psutil
+import time
+from threading import Thread
+from werkzeug.serving import make_server
 
 app = Flask(__name__)
 
-def get_ip_address():
-    hostname = socket.gethostname()
-    return socket.gethostbyname(hostname)
-
-def get_running_processes():
-    return subprocess.getoutput('ps -ax')
-
-def get_disk_space():
-    return subprocess.getoutput('df -h /')
-
-def get_uptime():
-    return subprocess.getoutput('uptime -p')
-
-@app.route('/')
-def service1_info():
-    service1_data = {
-        'Service1': {
-            'ip_address': get_ip_address(),
-            'running_processes': get_running_processes(),
-            'disk_space': get_disk_space(),
-            'uptime': get_uptime()
-        }
+@app.route("/service1")
+def service1_status():
+    # Get IP address
+    ip_address = socket.gethostbyname(socket.gethostname())
+    
+    # List running processes (limit to 5 for brevity)
+    processes = [proc.info for proc in psutil.process_iter(['pid', 'name'])][:5]
+    
+    # Disk space information
+    disk_usage = psutil.disk_usage('/')
+    disk_space = {
+        "total": disk_usage.total,
+        "used": disk_usage.used,
+        "free": disk_usage.free,
+        "percent": disk_usage.percent
     }
-
-    # Fetch data from Service2
-    try:
-        service2_response = requests.get('http://service2:5000')
-        service1_data['Service2'] = service2_response.json()
-    except Exception as e:
-        service1_data['Service2'] = str(e)
+    
+    # Uptime calculation
+    uptime_seconds = time.time() - psutil.boot_time()
+    uptime_formatted = time.strftime("%H:%M:%S", time.gmtime(uptime_seconds))
+    
+    # JSON response with system info
+    service1_data = {
+        "ip_address": ip_address,
+        "processes": processes,
+        "disk_space": disk_space,
+        "uptime_seconds": uptime_seconds,
+        "uptime": uptime_formatted
+    }
 
     return jsonify(service1_data)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8199)
+@app.route("/shutdown", methods=["POST"])
+def shutdown():
+    shutdown_server()
+    return "Service shutting down...", 200
+
+def shutdown_server():
+    func = request.environ.get("werkzeug.server.shutdown")
+    if func is None:
+        raise RuntimeError("Not running with the Werkzeug Server")
+    func()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)  # Listen on all interfaces and port 5000
